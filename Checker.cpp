c7e948b6345bde069a25f47f02ce44f6c25bf413
Sravaniseelam17 <Seelamsravani777@gmail.com>
2025-07-21 20:37:05 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -3,33 +3,34 @@
 #include <assert.h>
 
 bool isTemperatureOk(float temperature) {
-    return (temperature >= 0 && temperature <= 45);
+    bool ok = (temperature >= 0 && temperature <= 45);
+    if (!ok) {
+        printf("Temperature out of range!\n");
+    }
+    return ok;
 }
 
 bool isSocOk(float soc) {
-    return (soc >= 20 && soc <= 80);
+    bool ok = (soc >= 20 && soc <= 80);
+    if (!ok) {
+        printf("State of Charge out of range!\n");
+    }
+    return ok;
 }
 
 bool isChargeRateOk(float chargeRate) {
-    return (chargeRate <= 0.8);
+    bool ok = (chargeRate <= 0.8);
+    if (!ok) {
+        printf("Charge Rate out of range!\n");
+    }
+    return ok;
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-    bool status = true;
-
-    if (!isTemperatureOk(temperature)) {
-        printf("Temperature out of range!\n");
-        status = false;
-    }
-    if (!isSocOk(soc)) {
-        printf("State of Charge out of range!\n");
-        status = false;
-    }
-    if (!isChargeRateOk(chargeRate)) {
-        printf("Charge Rate out of range!\n");
-        status = false;
-    }
-    return status;
+    // Only 1 return, no branches in this function
+    return isTemperatureOk(temperature) &
+           isSocOk(soc) &
+           isChargeRateOk(chargeRate);
 }
 
 int main() {
