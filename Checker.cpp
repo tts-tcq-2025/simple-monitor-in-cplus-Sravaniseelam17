d5851dba47a0efbc313f744c9622253a8af1a5c5
Sravaniseelam17 <Seelamsravani777@gmail.com>
2025-07-21 20:32:30 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,22 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include <assert.h>
-#include <iostream>
-using namespace std;
+
+bool isTemperatureOk(float temperature) {
+    return (temperature >= 0 && temperature <= 45);
+}
+
+bool isSocOk(float soc) {
+    return (soc >= 20 && soc <= 80);
+}
+
+bool isChargeRateOk(float chargeRate) {
+    return (chargeRate <= 0.8);
+}
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+    bool status = true;
+
+    if (!isTemperatureOk(temperature)) {
+        printf("Temperature out of range!\n");
+        status = false;
+    }
+    if (!isSocOk(soc)) {
+        printf("State of Charge out of range!\n");
+        status = false;
+    }
+    if (!isChargeRateOk(chargeRate)) {
+        printf("Charge Rate out of range!\n");
+        status = false;
+    }
+    return status;
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    assert(batteryIsOk(25, 70, 0.7) == true);
+    assert(batteryIsOk(50, 85, 0) == false);
+    return 0;
 }
